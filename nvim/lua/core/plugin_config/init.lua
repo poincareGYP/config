0a72e6cd04e9cd0d3e010879bcc321aa1608778b
Aditya Kumar Singh <poincaregyp@gmail.com>
2024-04-20 15:54:33 +0530

    nvim : initial config

---
+++ nvim/lua/core/plugin_config/init.lua
@@ -0,0 +1,6 @@
+require ("core.plugin_config.gruvbox")
+require ("core.plugin_config.nvim_tree")
+require ("core.plugin_config.telescope")
+require ("core.plugin_config.treesitter")
+-- require ("core.plugin_config.nvim_web_devicons")
+
